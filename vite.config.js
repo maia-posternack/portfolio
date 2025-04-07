f2deb948d6d8e0f9db9b9ec282ba6250ce823c84
Maia Posternack <maiaposternack@gmail.com>
2025-04-06 23:59:42 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -15,4 +14,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: './', // Add this line to fix routing issues in production
 })
